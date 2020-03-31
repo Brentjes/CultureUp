b3313819e479b7235c619860d15de5a9bdaf7957
DoKM <JochemBoerGame2@gmail.com>
2020-03-31 18:07:53 +0200

    messed around with z-index to display the elements in the right order

--- public/editor.js
+++ public/editor.js
@@ -1,15 +1,7 @@
-// $(document).ready(function () {
-//     $('#sidebarCollapse').on('click', function () {
-//         $('#sidebar').toggleClass('active');
-//         $(this).toggleClass('active');
-//     });
-// });
-
 $(document).ready(function () {
-    document.getElementById('sidebarCollapse').addEventListener('click', sidebarButton);
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
 });
 
-function sidebarButton(){
-    $('#sidebar').toggleClass('active');
-    $(this).toggleClass('active');
-}
