8239821b4cae3b831a248921e0a0ff2251e57c0b
Jessy Besse <bess0003@hz.nl>
2020-04-06 11:21:06 +0200

    Added relationship between User and Teacher

--- database/migrations/2020_04_06_085306_create_teachers_table.php
+++ database/migrations/2020_04_06_085306_create_teachers_table.php
@@ -15,7 +15,12 @@ class CreateTeachersTable extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+
+                ->onDelete('cascade');
         });
     }
 
