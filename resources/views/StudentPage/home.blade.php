d056d1683f88c70ac9f0663518290e9e142e4153
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2020-04-06 15:43:10 +0200

    add studentpage to git, ready for database

--- resources/views/StudentPage/home.blade.php
+++ resources/views/StudentPage/home.blade.php
@@ -17,7 +17,7 @@
             <div class="row">
                 <div class="col-8">
 
-                    <div class="card text-center w-100 p-0 m-5 pr-0">
+                    <div class="card rounded text-center w-100 p-0 m-5 pr-0">
                         <div class="card-header d-block justify-content-center ribbon p-0">
                             <p class="ribbonText p-3 m-0">Progress</p>
                         </div>
@@ -44,7 +44,7 @@
                                             <p class="display-5 mb-0 p-2">#2 Chinese culture on the workfloor</p>
                                         </div>
                                         <div class="col">
-                                            <div  class="progress">
+                                            <div class="progress">
                                                 <div class="progress-bar progress-bar-striped bg-info"
                                                      role="progressbar" style="width: 25%" aria-valuenow="25"
                                                      aria-valuemin="0" aria-valuemax="100"></div>
@@ -64,7 +64,6 @@
                                                      role="progressbar" style="width: 75%" aria-valuenow="75"
                                                      aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
-
                                         </div>
                                     </div>
                                 </li>
@@ -74,13 +73,12 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="col-3 w-100  pr-0">
-                    <div class="card text-center w-100 p-0 m-5  pr-0">
-                        <div class="card-header d-block justify-content-center ribbon p-0">
+                <div class="col-3 w-100 pr-0">
+                    <div class="card rounded  text-center w-100 h-75 p-0 m-5  pr-0">
+                        <div class="card-header d-block justify-content-center ribbon p-0 ">
                             <p class="ribbonText p-3 m-0">News Feed</p>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body ">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">Cras justo odio</li>
                                 <li class="list-group-item">Dapibus ac facilisis in</li>
