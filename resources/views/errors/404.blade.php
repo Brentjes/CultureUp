0800e78a527b2878fa92b15bfe0320ced58e40a1
tilb0023 <tilb0023@hz.nl>
2020-05-08 11:26:41 +0200

    add 500 page
    todo: fix darkmode issue

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends ('layouts.layout')
+@extends ('layouts.errorLayout')
 @section ('content')
 
     <head>
@@ -11,37 +11,37 @@
             <div class="row">
                 <div class="card rounded text-center w-100 h-75 p-0 mt-5">
                     <div class="card-header d-block justify-content-center ribbon p-0">
-                        <p class="ribbonText p-3 m-0">We could not find your destination, sorry!</p>
+                        <p class="ribbonText p-3 m-0">Oops we've landed somewhere that doesn't exist, sorry!</p>
                     </div>
                     <div class="card-body">
                         <div class="row d-flex justify-content-left">
-                            <h3 class="pt-3 pl-5 p-1 m-0" style="color: rgba(69,86,95,0.35)">Error:</h3>
+                            <h3 class="pt-3 pl-5 p-1 m-0 error-text">Error:</h3>
                             <h3 class="pt-3 p-1 m-0" style="color: rgba(35,156,205,0.69)">404</h3>
                         </div>
                         <div class="row d-flex justify-content-left">
-                            <h2 class="pl-5 m-0">Try these destinations instead:</h2>
+                            <h2 class="pl-5 m-0">Want to fly here instead?</h2>
                         </div>
                         <div class="row center-row text-left p-5">
                             <div class="col">
                                 <a href="/home">
-                                    <p class="">-home</p>
+                                    <p>-home</p>
                                 </a>
-                                <a href="/home">
+                                <a href="/assignment">
                                     <p>-assignment</p>
                                 </a>
-                                <a href="/home">
-                                    <p>-home</p>
+                                <a href="/notifications">
+                                    <p>-notifications</p>
                                 </a>
-                                <a href="/home">
-                                    <p>-home</p>
+                                <a href="/leaderboard">
+                                    <p>-leaderboard</p>
                                 </a>
-                                <a href="/home">
-                                    <p>-home</p>
+                                <a href="/logout">
+                                    <p>-logout</p>
                                 </a>
                             </div>
-{{--                            <div class="col mr-20 text-right mt-5 mb-5">--}}
-{{--                                <img id="logo" class="img-fluid switch" style="width:25vw; height: auto" src="/images/logoLight.png">--}}
-{{--                            </div>--}}
+                            <div class="col pr-5 text-right ">
+                                <img class="img-fluid switch" style="width:25vw; height: auto;" src="/images/placeholderPlane.png">
+                            </div>
                         </div>
                     </div>
                 </div>
