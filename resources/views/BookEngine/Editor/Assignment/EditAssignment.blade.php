0d2d1dc4d1aca961ea168a6902b4256d173a0483
DoKM <mcazzyman@gmail.com>
2020-05-11 09:47:08 +0200

    edit edit assignment page

--- resources/views/BookEngine/Editor/Assignment/EditAssignment.blade.php
+++ resources/views/BookEngine/Editor/Assignment/EditAssignment.blade.php
@@ -1,82 +1,164 @@
-@foreach ($errors->all() as $error)
-    <p>{{ $error }}</p>
-@endforeach
+<!DOCTYPE html>
+<html>
 
-<Form method="POST" action="/assignment/editor/current/{{$assignment->id}}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>CultureUp - Editor</title>
 
-    @csrf
 
-    @method('PUT')
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
-    <div class="field">
-        <label class="label" for="title">Title</label>
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="/css/BookEngine/editor.css">
 
-        <div class="control">
-            <div class="control">
-                <input class="input" type="text" name="title" id="title" required value="{{old("title")}}">
-            </div>
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/5b65a26ca8.js" crossorigin="anonymous"></script>
+</head>
+<body>
 
-            @error('title')
-            <p>{{$errors->first('title')}}</p>
-            @enderror
-
+<div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar" style="z-index: 100">
+        <div class="sidebar-header">
+            <h3>CultureUP Editor</h3>
         </div>
-    </div>
 
-    <div class="field">
-        <label class="label" for="subject">Subject</label>
+        <ul class="list-unstyled components">
+            <li class="softshadow">
+                <a href="#"><i class="fas fa-quote-right"></i> Add Text</a>
+            </li>
+            <li class="softshadow">
+                <a href="#"><i class="fas fa-image"></i> Add Image</a>
+            </li>
+            <li class="softshadow">
+                <a href="#"><i class="fas fa-link"></i> Add Link</a>
+            </li>
+            <li class="softshadow">
+                <a href="#"><i class="fas fa-heading"></i> Change Title</a>
+            </li>
+            <li class="softshadow">
+                <a href="#"><i class="fas fa-paragraph"></i> Change Description</a>
+            </li>
+            <li>
+                <input type="button" id="updateAssignmentButton">
+            </li>
+            <li>
+                <input type="text" id="assignmentName">
+            </li>
+            <li>
+                <input type="text" id="assignmentSubject">
+            </li>
+            <li>
+                <label for="testButton">NewPage</label>
+                <input name="testButton" type="button" id="testButton">
+            </li>
+            <li id="testAppend">
+            </li>
 
-        <div class="control">
-            <div class="control">
-                <input class="input" type="text" name="subject" id="subject" value="{{old("subject")}}" required>
-            </div>
+        </ul>
 
-            @error('subject')
-            <p>{{$errors->first('subject')}}</p>
-            @enderror
-
+    </nav>
+    <nav id="bottombar" style="z-index: 100">
+        <div class="sidebar-header text-center pt-2">
+            <h3>Timeline</h3>
         </div>
+
+        <button type="button" id="bottombarCollapse" class="btn">
+            <i class="white-icon fas fa-photo-video"></i>
+        </button>
+    </nav>
+    <!-- Page Content -->
+    <div id="content">
+        <button type="button" id="sidebarCollapse" class="btn">
+            <i class="white-icon fas fa-pencil-ruler"></i>
+        </button>
     </div>
-
-    <div class="field">
-        <label class="label" for="isHidden">isHidden</label>
-
-        <input class="input" type="checkbox" name="isHidden"
-               @if( (old('isHidden') ) == (true||1) ) checked @endif
-               id="isHidden">
-
-        @error('isHidden')
-        <p>{{$errors->first('isHidden')}}</p>
-        @enderror
-
-    </div>
-
-    <div class="field">
-        <label class="label" for="isLocked">isLocked</label>
-
-        <input class="input" type="checkbox" name="isLocked" id="isLocked"
-               @if( (old('isLocked') ) == (true||1) ) checked @endif>
-
-        @error('isLocked')
-        <p>{{$errors->first('isLocked')}}</p>
-        @enderror
-
-    </div>
+</div>
 
 
 
+<div id="ElementList">
 
-    <div class="field is-grouped">
+</div>
+
+<meta>
+
+<script>
+
+    document.getElementById('updateAssignmentButton').addEventListener("click", sendAssignmentUpdate);
+
+    function sendAssignmentUpdate() {
+        let assignmentUpdateUrl = "{{ route('editor.current.update', $assignment->id) }}";
+        let body =  {
+            name: document.getElementById('assignmentName').value,
+            subject: document.getElementById('assignmentSubject').value,
+        };
+
+        if (!(body.name.length > 0 && body.name.length <= 50 && body.subject.length > 0 && body.subject.length <= 200)) {
+            console.log("one of these inputs are to long, please shorten them, title max length = 50, subject max length = 200");
+            if (!(body.name.length > 0 && body.name.length <= 50)) {
+                console.log('name');
+                console.log(body.name.length);
+            }
+            if (!(body.subject.length > 0 && body.subject.length <= 200)) {
+                console.log('subject');
+                console.log(body.subject.length);
+            }
+
+            return
+        }
+
+        sendFetchTo(assignmentUpdateUrl, body, 'put');
+
+    }
+
+    function sendFetchTo(url, body, method){
+        console.log(method);
+        console.log(body);
+        console.log(url);
+        fetch(url, {
+            method: method,
+            credentials: "same-origin",
+            body: JSON.stringify(body),
+            mode: 'cors',
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": document.head.querySelector("[name~=csrf-token][content]").content
+            }
+
+        })
+            // .then( (response) => return response.text());
+            .then( (text) => console.log(text))
+            .catch( (error) => console.log(error) );
+    }
+
+</script>
 
 
-        <div class="control">
-            <button class="button is-link buttongreen center" type="submit">Submit</button>
-        </div>
-        <div class="control">
-            <button class="button is-text center">cancel</button>
-        </div>
-    </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"--}}
+{{--        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"--}}
+{{--        crossorigin="anonymous"></script>--}}
+<script src="/editor.js"></script>
 
-</Form>
 
-{{$assignment->id}}
+</body>
+
+</html>
+
+
+
+
+
