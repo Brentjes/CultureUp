012bbc752610e27ff389a4614ffe27f66a08376a
tilb0023 <tilb0023@hz.nl>
2020-05-12 13:58:31 +0200

    add link to editor

--- resources/views/BookEngine/Editor/Assignment/EditAssignment.blade.php
+++ resources/views/BookEngine/Editor/Assignment/EditAssignment.blade.php
@@ -145,45 +145,47 @@
 
     <div class="row position-absolute" style="margin-left: 15%; width: 85vw;">
         @foreach($assignment->pages as $page)
-        <div class="col-auto p-3">
-            <div class="card text-white custom-rounded softshadow" style="width: 12vw;">
-                <img class="card-img custom-rounded" src="http://placehold.it/160x90">
-                <div class="card-img-overlay">
-                    <h2 class="card-title" style="font-size: 1vw;">{{$page->name}}</h2>
-                </div>
+            <div class="col-auto p-3">
+                <a href="{{ route('editor.page.show', ['assignmentID' => $assignment->id, 'page' => $page->id]) }}">
+                    <div class="card text-white custom-rounded softshadow" style="width: 12vw;">
+                        <img class="card-img custom-rounded" src="http://placehold.it/160x90">
+                        <div class="card-img-overlay">
+                            <h2 class="card-title" style="font-size: 1vw;">{{$page->name}}</h2>
+                        </div>
+                    </div>
+                </a>
             </div>
-        </div>
         @endforeach
 
-</div>
+    </div>
 
-<div id="ElementList">
+    <div id="ElementList">
 
-</div>
+    </div>
 
-<meta>
+    <meta>
 
-<script>
-    @include("BookEngine.Editor.js.assignmentEdit")
-    @include("BookEngine.Editor.js.newPage")
-</script>
+    <script>
+        @include("BookEngine.Editor.js.assignmentEdit")
+        @include("BookEngine.Editor.js.newPage")
+    </script>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-<!-- Popper.JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
-        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
-        crossorigin="anonymous"></script>
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
-        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
-        crossorigin="anonymous"></script>
-{{--Bootstrap select--}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+            integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+            crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+            integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+            crossorigin="anonymous"></script>
+    {{--Bootstrap select--}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 
-<script src="/editor.js"></script>
+    <script src="/editor.js"></script>
 
 
 </body>
