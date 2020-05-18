ea2129a9d3540e9c1f6069c0c5d5e4caba637627
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2020-05-18 09:44:08 +0200

    fix bug regarding nightmode, navbar and layout

--- resources/views/StudentPage/profile.blade.php
+++ resources/views/StudentPage/profile.blade.php
@@ -2,147 +2,145 @@
 @extends ('layouts.profile')
 @section ('profile')
 
-    <div id="studentpage">
-        <div class="container">
+
+    <div class="card rounded text-center w-100 p-0 m-5">
+        <div class="card-header d-block justify-content-center ribbon p-0">
             <div class="row">
-                <div class="col-5">
+                <div class="col">
+                    <p class="ribbonText text-center p-3 m-0">You</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <li class="list-group-item">
+                <div class="img-fluid pt-4 pb-5">
+                    <img class="rounded" src="/images/default.jpg"
+                         style="width: 15vw; height: auto;">
+                </div>
+                <div class="row center-row text-left">
+                    <div class="col">
+                        <p class="display-5 display-sm-3 display-m-4 display-md-5 mb-0">
+                            <b>Name:</b> {{$user->name}}</p>
+                        <p class="display-5 mb-0"><b>Teacher:</b> Bugs Bunny</p>
+                        <p class="display-5 mb-0"><b>Country:</b> Bosnia Herzegovina</p>
 
-                    <div class="card rounded text-center w-100 p-0 m-5">
-                        <div class="card-header d-block justify-content-center ribbon p-0">
-                            <div class="row">
-                                <div class="col">
-                                    <p class="ribbonText text-center p-3 m-0">You</p>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+                <div class="row pt-4 center-row text-left">
+                    <div class="col">
+                        <p class="display-5 mb-0"><b>Course:</b> English</p>
+                    </div>
+                </div>
+            </li>
+        </div>
+    </div>
+    </div>
+
+    <div class="col-7 h-100 pl-5">
+        <div class="card rounded text-center w-100 h-75 p-0 m-5">
+            <div class="card-header d-block justify-content-center ribbon p-0 ">
+                <p class="ribbonText p-3 m-0">Badges</p>
+            </div>
+            <div class="card-body">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/the netherlands.png">
                         </div>
-                        <div class="card-body">
-                            <li class="list-group-item">
-                                <div class="img-fluid pt-4 pb-5">
-                                    <img class="rounded" src="/images/default.jpg"
-                                         style="width: 15vw; height: auto;">
-                                </div>
-                                <div class="row center-row text-left">
-                                    <div class="col">
-                                        <p class="display-5 display-sm-3 display-m-4 display-md-5 mb-0">
-                                            <b>Name:</b> {{$user->name}}</p>
-                                        <p class="display-5 mb-0"><b>Teacher:</b> Bugs Bunny</p>
-                                        <p class="display-5 mb-0"><b>Country:</b> Bosnia Herzegovina</p>
-
-                                    </div>
-                                </div>
-                                <div class="row pt-4 center-row text-left">
-                                    <div class="col">
-                                        <p class="display-5 mb-0"><b>Course:</b> English</p>
-                                    </div>
-                                </div>
-                            </li>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/belgium.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/germany.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/austria.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/japan.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/china.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/croatia.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/rwanda.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/greece.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/russia.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/belarus.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/sweden.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/finland.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/ukraine.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/bosnia and herzegovina.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/taiwan.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/mongolia.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/tanzania.png">
+                        </div>
+                    </div>
+                    <div class="col-auto text-left p-3">
+                        <div class="img-fluid">
+                            <img class="flag softshadow" src="/flags/PNG/70/egypt.png">
                         </div>
                     </div>
                 </div>
-                <div class="col-7 h-100 pl-5">
-                    <div class="card rounded text-center w-100 h-75 p-0 m-5">
-                        <div class="card-header d-block justify-content-center ribbon p-0 ">
-                            <p class="ribbonText p-3 m-0">Badges</p>
-                        </div>
-                        <div class="card-body">
-                            <div class="row d-flex justify-content-center">
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/the netherlands.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/belgium.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/germany.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/austria.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/japan.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/china.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/croatia.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/rwanda.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/greece.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/russia.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/belarus.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/sweden.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/finland.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/ukraine.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/bosnia and herzegovina.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/taiwan.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/mongolia.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/tanzania.png">
-                                    </div>
-                                </div>
-                                <div class="col-auto text-left p-3">
-                                    <div class="img-fluid">
-                                        <img class="flag softshadow" src="/flags/PNG/70/egypt.png">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,30 +1,19 @@
 @section ('content')
-<head>
-    <link rel="stylesheet" type="text/css" href="/css/teacher/profile.css">
-    <title>CultureUp - Profile</title>
-</head>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/teacher/profile.css">
+        <title>CultureUp - Profile</title>
+    </head>
 
-<section id="studentpage">
-    <div class="container">
-        <div class="row">
-            <div class="col-5">
+    <section id="studentpage">
+        <div class="container">
+            <div class="row">
 
-                <div class="card rounded text-center w-100 p-0 m-5">
-                    <div class="card-header d-block justify-content-center ribbon p-0">
-                        <div class="row">
-                            <div class="col">
-                                <p class="ribbonText text-center p-3 m-0">You</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <li class="list-group-item">
-                            <div class="img-fluid pt-4 pb-5">
-                                <img class="rounded" src="/images/default.jpg"
-                                     style="width: 15vw; height: auto;">
-                            </div>
-                        @yield ('profile')
-                    </div>
+                <div class="col-5">
+
+                    @yield ('profile')
                 </div>
-</section>
+            </div>
+        </div>
+
+    </section>
 @endsection
