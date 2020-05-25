96630326861e22960406b95d4d4c297aec1c4832
wongus <wongus.in@hotmail.com>
2020-05-25 16:02:15 +0200

    fix editor

--- resources/views/BookEngine/Editor/editor.blade.php
+++ resources/views/BookEngine/Editor/editor.blade.php
@@ -58,8 +58,7 @@
     <!-- Bottombar -->
     <nav id="bottombar" style="z-index: 100">
         <div class="text-center pt-2 sidebar-header">
-            <h3 class="d-inline" style="color: #212529;">Timeline</h3><i
-                class="d-inline ml-2 fas fa-ellipsis-h bar-icon" style="color: #323232; margin: 0px"></i>
+            <h3 class="d-inline" style="color: #212529;">Timeline</h3>
         </div>
         <div class="row w-100 text-center ml-2 pl-5" style="height: 200px; margin-top: -3px">
             <div class="col-auto p-3 my-auto">
