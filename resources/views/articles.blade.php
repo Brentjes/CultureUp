8e03181e6b436b281898e61a96083cfda94aaf1c
Leslie <Sche0171@hz.nl>
2020-05-29 11:09:30 +0200

    Private articles now show in dropdown

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -26,14 +26,17 @@
                 @endforeach
             </div>
     </section>
+
+
     <section id="overlay">
-        <div class="container text-center expand">
-            <button id="viewMore" type="button text-center" class="btn btn-outline-secondary" style="margin-top: 2px;"><span
+        <div class="container-fluid text-center expand">
+            <button id="viewMore" type="button text-center" class="btn btn-outline-secondary"
+                    style="margin-top: 2px;"><span
                     class="material-icons btn">expand_more</span>
                 View more
             </button>
             <hr>
-            <h2 class="display-3">Lorem Ipsum <br>
+            <h2 class="display-3">Your Articles<br>
 
 
                 <button type="button text-center" class="btn btn-outline-info">
@@ -42,15 +45,27 @@
                     </a>
                 </button>
             </h2>
-            <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc rhoncus nisl eu pharetra
-                convallis.
-               <br><br>
 
-                Vestibulum commodo in lorem at gravida. Suspendisse feugiat in risus et mollis. In magna arcu, facilisis
-                in nunc sed, pulvinar ullamcorper lorem. Nulla facilisi. Suspendisse accumsan enim quis mi pretium
-                tristique nec in eros. Donec quis nulla nec felis pellentesque fermentum. Phasellus faucibus laoreet
-                ante vel scelerisque. Sed ut tincidunt purus. Phasellus euismod ipsum velit, eget fermentum justo
-                molestie sed. Aenean non condimentum justo, auctor pulvinar tortor.</p>
+            <div class="cards">
+                <div class="wow fadeInUp card-columns d-inline-block">
+                    @foreach ($articles->where('user_id' , Auth::user()->id) as $article)
+                        <i href="{{ route('article.show', $article -> id) }}" class="custom-card">
+                            <div class="card rounded text-center h-auto rounded softshadow">
+                                <i class="fas fa-pencil-alt float-right m-2"></i>
+                                <img class="img-fluid rounded" src="https://picsum.photos/id/{{$article->id}}/700">
+                                <div class="card-body pb-0">
+                                    <div class="col text-left p-0">
+                                        <h4 class="h4">{{$article->title}}</h4>
+                                        <blockquote class="blockquote">
+                                            <p class="h6 lead">{{$article->excerpt}}</p>
+                                        </blockquote>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </section>
 @endsection
