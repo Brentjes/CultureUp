7a65f136950edb22e3b6ac2953a0e7246d0d92f7
wongus <wongus.in@hotmail.com>
2020-06-04 13:40:43 +0200

    add new forms

--- resources/views/BookEngine/Editor/Element/CreateText.blade.php
+++ resources/views/BookEngine/Editor/Element/CreateText.blade.php
@@ -1,1 +1,30 @@
+<div id="content">
+    <div class="assignment-info mt-5 pt-5 container d-flex justify-content-center" id="imageForm">
+        <div class="card rounded p-0">
+            <div class="card-header ribbon p-0">
+                <p class="ribbonText text-center p-3 m-0">Add text</p>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('element.store', ['assignment' => 23, 'page' => 11]) }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="alt-text">Text</label>
+                        <input class="form-control" name="text" placeholder="Insert text here">
+                        @error('alt-text')
+                        <p>{{$errors->first('alt-text')}}</p>
+                        @enderror
+                    </div>
 
+                    <div class="row text-center pt-4">
+                        <div class="col">
+                            <button class="btn btn-danger">Cancel</button>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-light" type="submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
